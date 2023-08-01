13c7a414f459d5fc3fed2d10e355a4214908ece6
balavignesh111 <balavignesh1018@gmail.com>
2023-08-01 22:54:56 +0530

    as of now 5 problems completed

--- script.js
+++ script.js
@@ -1,1 +1,125 @@
 'use script';
+
+// first problem
+const calculate = function(){
+  const resultEle = document.getElementById('result');
+  const numOne = document.getElementById('num1');
+  const operator = document.getElementById('operator');
+  const numTwo = document.getElementById('num2');
+  if(numOne.value === ''){
+    resultEle.innerText = `please type first number`;
+  }else if(operator.value === ''){
+    resultEle.innerText = `please type operator input`;
+  }else if(numTwo.value === ''){
+    resultEle.innerText = `please type second number`;
+  }else{
+    const value = eval(numOne.value+operator.value+numTwo.value);
+  resultEle.innerText = `Answer for first problem is ${value}`;
+  }
+}
+
+// second problem
+const quotesArray = [`“We cannot solve problems with the kind of thinking we employed when we came up with them.” — Albert Einstein`,
+`“Learn as if you will live forever, live like you will die tomorrow.” — Mahatma Gandhi`,
+`“Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too.” — Mark Twain`,
+`“When you give joy to other people, you get more joy in return. You should give a good thought to happiness that you can give out.”— Eleanor Roosevelt`
+];
+const generateQuote = () =>{
+  // random no is generated
+  const randomNumber = Math.trunc(Math.random()*quotesArray.length);
+  // dynamic element is created
+  const secondQuestionElement = document.querySelector('.question2');
+  const quoteElement = document.createElement('p');
+  quoteElement.setAttribute('id','result');
+  quoteElement.innerText = quotesArray[randomNumber];
+  // logic to append child and to rmv if already presented..
+  if(secondQuestionElement.childNodes.length === 7){
+    secondQuestionElement.appendChild(quoteElement);  
+  }else{
+    secondQuestionElement.removeChild(secondQuestionElement.lastElementChild);
+    secondQuestionElement.appendChild(quoteElement); 
+  }
+}
+
+// third problem
+
+// gv
+const pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; 
+const validateForm = ()=>{
+  const nameEle = document.getElementById('name');
+  const emailEle = document.getElementById('email');
+  const errorEle = document.getElementById('error');
+  if(nameEle.value == '' && emailEle.value == ''){
+    errorEle.innerText = `Enter your name and email id`;
+  }else if(nameEle.value === ''){
+    errorEle.innerText = `Enter your name`;
+  }else if(emailEle.value === ''){
+    errorEle.innerText = `Enter your email`;
+  }else if(!(emailEle.value.match(pattern))){
+    errorEle.innerText = `Invalid email format`;
+  }else{
+    errorEle.innerText = `all input fields are in correct format`;
+  }
+}
+
+// 4 problem
+const calculateTypingSpeed = ()=>{
+  // setTimeout(()=>{
+  //   console.log('aa');
+  // },1000*60) 
+}
+
+// 5 problem
+// 1. if it is less than 5 -> week, between 5 and 10 -> moderate, more 10 it is -> strong
+const checkPasswordStrength = ()=>{
+  const passwordEle = document.getElementById('password');
+  const resultEle = document.getElementById('passwordStrength');
+  resultEle.innerText = ((passwordEle.value.length <= 5) ? `weak password` : (passwordEle.value.length <= 10 ) ? `moderate password` : `strong password`);
+}
+
+// 6. problem
+
+// gv
+let index = 0;
+const imageContainerEle = document.querySelectorAll('.image-slider img');
+console.log(imageContainerEle)
+// func
+const prevSlide = ()=>{
+  index--;
+  for(let i=0;i<imageContainerEle.length;i++){
+    imageContainerEle[i].style.display = "none";
+  }
+  if(index < 0){
+    index = imageContainerEle.length-1;
+    imageContainerEle[index].style.display = "inline";
+  }else if(index <= imageContainerEle.length -1 ){
+    imageContainerEle[index].style.display = "inline";
+  }
+}
+
+const nextSlide = ()=>{
+  index++;
+  for(let i=0;i<imageContainerEle.length;i++){
+    imageContainerEle[i].style.display = "none";
+  }
+  if(index === imageContainerEle.length){
+    index = 0;
+    imageContainerEle[index].style.display = "inline";
+  }else if(index > 0){
+    imageContainerEle[index].style.display = "inline";
+  }
+}
+
+const toggleAutoPlay = ()=>{
+  setInterval(()=>{
+    for(let i=0;i<imageContainerEle.length;i++){
+      imageContainerEle[i].style.display = "none";
+    }
+    if(index < imageContainerEle.length-1){
+      index++;
+    }else{
+      index = 0;
+    }
+    imageContainerEle[index].style.display = "inline";
+  },2000);
+}
